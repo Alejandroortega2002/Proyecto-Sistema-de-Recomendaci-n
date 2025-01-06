4880316a712020145fe4499bb395d098f5a090db
cuatrobrazos1 <m.marchalmalla@gmail.com>
2025-01-06 18:48:43 +0100

    Funcionalidad de las votaciones terminada Modelo y Controlador ajustados al nuevo metodo

--- controlador/Controlador_peliculas.py
+++ controlador/Controlador_peliculas.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication
 from vista.Vista_principal import Vista_principal
 from modelo.Modelo_peliculas import Modelo_peliculas
 
+
 class Controlador_peliculas:
     def __init__(self, app):
         self.app = app
@@ -9,6 +10,7 @@ class Controlador_peliculas:
         self.principal_view = Vista_principal()
 
         self.principal_view.conectar_busqueda(self.buscar_peliculas)
+        self.principal_view.conectar_votacion(self.votaciones)
         self.cargar_peliculas_azar()
 
     def buscar_peliculas(self):
@@ -19,6 +21,14 @@ class Controlador_peliculas:
     def cargar_peliculas_azar(self):
         peliculas = self.peliculas_model.peliculas_azar()
         self.principal_view.mostrar_peliculas_azar(peliculas)
+
+    def votaciones(self):
+        nombre_pelicula = self.principal_view.pelicula_input.text()
+        puntuacion = int(self.principal_view.puntuacion_input.text())
+        username = self.principal_view.username_input.text()
+
+        self.peliculas_model.votaciones(nombre_pelicula, puntuacion, username)
+        self.principal_view.mostrar_mensaje(f"Votación guardada para {username} en la película '{nombre_pelicula}'")
 
     def run(self):
         self.principal_view.show()
--- modelo/Modelo_peliculas.py
+++ modelo/Modelo_peliculas.py
@@ -1,10 +1,14 @@
 
 import pandas as pd
+import ast
 
 class Modelo_peliculas:
+
     def __init__(self):
-        self.file_path = 'peliculas_final.csv'
-        self.peliculas_df = pd.read_csv(self.file_path)
+        self.file_path_peliculas = 'peliculas_final.csv'
+        self.file_path_usuarios = 'usuarios.csv'
+        self.peliculas_df = pd.read_csv(self.file_path_peliculas)
+        self.usuarios_df = pd.read_csv(self.file_path_usuarios)
 
     def sacar_peliculas(self, nombre_pelicula):
         resultado = self.peliculas_df[self.peliculas_df['title'].str.contains(nombre_pelicula, case=False, na=False)]
@@ -19,3 +23,27 @@ class Modelo_peliculas:
     def peliculas_azar(self):
         muestra = self.peliculas_df.sample(n=12, random_state=123)
         return muestra[['title', 'Imagen']].values.tolist()
+
+    def votaciones(self, NombrePelicula, puntuacion, username):
+        if username in self.usuarios_df["Nombre de usuario"].values:
+            usuario_row = self.usuarios_df[self.usuarios_df["Nombre de usuario"] == username].iloc[0]
+            if pd.isna(usuario_row["votaciones"]):
+                votaciones_usuario = []
+            else:
+                votaciones_usuario = ast.literal_eval(usuario_row["votaciones"])
+
+            pelicula_existente = next((v for v in votaciones_usuario if v['title'] == NombrePelicula), None)
+
+            if pelicula_existente:
+                pelicula_existente['rating'] = puntuacion
+            else:
+                votaciones_usuario.append({"title": NombrePelicula, "rating": puntuacion})
+
+            self.usuarios_df.loc[self.usuarios_df["Nombre de usuario"] == username, "votaciones"] = str(
+                votaciones_usuario)
+        else:
+            nuevas_votaciones = [{"title": NombrePelicula, "rating": puntuacion}]
+            nuevas_filas = pd.DataFrame([{"Nombre de usuario": username, "votaciones": str(nuevas_votaciones)}])
+            self.usuarios_df = pd.concat([self.usuarios_df, nuevas_filas], ignore_index=True)
+
+        self.usuarios_df.to_csv(self.file_path_usuarios, index=False)
